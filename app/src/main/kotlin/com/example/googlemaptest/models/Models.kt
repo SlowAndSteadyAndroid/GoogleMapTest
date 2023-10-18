1e6f7e156265edced61ce2261a215db4234bf0f5
Parkduksung <duksung1234@naver.com>
2023-10-18 21:55:59 +0900

    MP1Test test1 통과.

--- app/src/main/kotlin/com/example/googlemaptest/models/Models.kt
+++ app/src/main/kotlin/com/example/googlemaptest/models/Models.kt
@@ -8,6 +8,52 @@ class Place(
     val description: String = ""
 )
 
-fun List<Place>.search(keyword : String) : List<Place> {
-    return this
+fun List<Place>.search(keyword: String): List<Place> {
+
+    if (keyword.trim().isEmpty())
+        return this
+
+    val convertList = mutableListOf<Place>()
+
+
+    for (place in this) {
+
+        val descriptionList = place.description.lowercase().split(" ")
+
+        for (string in descriptionList) {
+            if (string.lowercase() == keyword.lowercase().trim()) {
+                convertList.add(place)
+                break
+            }
+
+            if (!string.chars().allMatch(Character::isAlphabetic)) {
+
+                var convertString = ""
+
+                string.toCharArray().forEach { c ->
+                    if (Character.isAlphabetic(c.code)) {
+                        convertString += c
+                    } else {
+                        convertString += if (c == '.' || c == ',' || c == '!' || c == '?' || c == ';' || c == ':'
+                            || c == '/'
+                        ) {
+                            " "
+                        } else {
+                            ""
+                        }
+                    }
+                }
+
+                convertString.split(" ").forEach {word ->
+                    if (word.lowercase() == keyword.lowercase().trim()) {
+                        convertList.add(place)
+                        return@forEach
+                    }
+                }
+            }
+        }
+
+    }
+
+    return convertList
 }

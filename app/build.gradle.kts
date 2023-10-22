8baf74bf338ddc21a556df0ad44e4506ff25e928
Parkduksung <duksung1234@naver.com>
2023-10-23 08:54:54 +0900

    room 추가.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,9 +88,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
 
     //Room
-    implementation("androidx.room:room-runtime:2.3.0")
-    kapt("androidx.room:room-compiler:2.3.0")
+    implementation("androidx.room:room-runtime:2.4.2")
+    kapt("androidx.room:room-compiler:2.4.2")
     kapt("org.xerial:sqlite-jdbc:3.34.0")
+    implementation("androidx.room:room-ktx:2.4.2")
 
     //ktx
     implementation("androidx.fragment:fragment-ktx:1.6.1")
